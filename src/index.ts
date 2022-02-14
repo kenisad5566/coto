2b11a590f95182aa194d02146201f0bb59beca7d
Beetle <819579706@qq.com>
2022-02-14 19:48:30 +0800

    feat: export bloom and lru

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from "./load/adaptive-shedder";
 export * from "./collection/rolling-window";
+export * from "./collection/lru";
+export * from "./bloom/bloom";
 export * from "./cpu/cpu";
 export * from "./type";
