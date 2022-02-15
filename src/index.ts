01eea31a5b74b559bc4688622c83b17952cb98e6
Beetle <819579706@qq.com>
2022-02-15 13:48:41 +0800

    feat: export lru-redis-zset

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./load/adaptive-shedder";
 export * from "./collection/rolling-window";
 export * from "./collection/lru";
+export * from "./collection/lru-redis-zset";
 export * from "./bloom/bloom";
 export * from "./cpu/cpu";
 export * from "./type";
